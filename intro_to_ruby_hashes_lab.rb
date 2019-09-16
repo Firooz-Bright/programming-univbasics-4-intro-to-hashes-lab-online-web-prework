d4dad1b299ba08c4dd99ed50efa9db845a7aaaa7
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-16 17:27:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
    new_one[:key]=value
-   return new_one
+   
 end
 
 def read_from_hash(hash, key)
