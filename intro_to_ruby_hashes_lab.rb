23a6c8ca47114c84ae18afb6a01149f31163a662
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-16 17:26:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash_made={ :hash => key}
-  return hash_made[:hash_made]
+  return hash[:key]
 end
 
 def update_counting_hash(hash, key)
